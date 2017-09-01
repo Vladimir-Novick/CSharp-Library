6e4d58e3155569765ff2017910106610eae14f03
Vladimir Novick <vlad.novick@gmail.com>
2017-08-31 20:20:49 -0700

    PostgreSQL utilities

---
+++ DBUtils/SGcomboDbHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Npgsql;
+using NpgsqlTypes;
+
+using System.Data;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using SGcomboLib.Npgsql.Config;
+
+////////////////////////////////////////////////////////////////////////////
+//	Copyright 2017 : Vladimir Novick    https://www.linkedin.com/in/vladimirnovick/  
+//
+//    NO WARRANTIES ARE EXTENDED. USE AT YOUR OWN RISK. 
+//
+// To contact the author with suggestions or comments, use  :vlad.novick@gmail.com
+//
+////////////////////////////////////////////////////////////////////////////
+
+
+namespace SGcomboLib.Npgsql.DBUtils
+{
+    public class SGcomboDbHelper
+    {
+        public static  NpgsqlConnection OpenConnection()
+        {
+            NpgsqlConnection MyNpgsqlConnection = new NpgsqlConnection();
+            string StrConnect = SGcomboConfigProvider.GetConnectionString();
+
+            MyNpgsqlConnection.ConnectionString = StrConnect;
+            MyNpgsqlConnection.Open();
+            return MyNpgsqlConnection;
+        }
+
+        
+
+        private static  NpgsqlCommand CreateSPCommand(string SP_Name, NpgsqlConnection MyNpgsqlConnection)
+        {
+            NpgsqlCommand MyNpgsqlCommand = MyNpgsqlConnection.CreateCommand();
+            MyNpgsqlCommand.CommandType = CommandType.StoredProcedure;
+
+            MyNpgsqlCommand.CommandText = SP_Name;
+            return MyNpgsqlCommand;
+        }
+
+
+
+    }
+
+
+}
